de07f1048bb8270cd2fbcff48d17744f76f2fa34
virat <105363781+Virat18v@users.noreply.github.com>
2022-09-12 10:04:51 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,4 @@
+import numpy
+speed = [20, 32, 11, 15, 77, 77, 20, 20]
+x = numpy.average(speed)
+print(x)
---
+++ mean.py
@@ -0,0 +1,4 @@
+import numpy
+velocity = [45, 60, 25, 35, 41, 24]
+x = numpy.mean(velocity)
+print(x)
